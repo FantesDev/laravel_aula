b960e3d7335d31a1ced1a3cb659ece75bfebd30e
Fantes-Dev <vitorhenriquefantes81@gmail.com>
2024-08-28 17:54:08 -0300

    :sparkles: feat: insert do formulário

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
         return View('site.contato', ['pagina' => 'Página de ']);
+
+            $contato = new Contato();
+            $contato->nome = $request->input('nome');
+            $contato->email = $request->input('email');
+            $contato->telefone = $request->input('telefone');
+            $contato->motivo_contato = $request->input('motivo_contato');
+            $contato->mensagem = $request->input('mensagem');
     }
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -3,6 +3,10 @@
 <form action="{{ route('site.contato') }}" method="post" class="{{ $dark }} p-4 border rounded shadow-sm">
     @csrf
     <h2 class="mb-4">Formulário de Contato</h2>
+    <div class="mb-3">
+        <label for="nome" class="form-label text-white">Nome Completo</label>
+        <input type="text" class="form-control" id="nome" name="nome" required>
+    </div>
 
     <div class="mb-3">
         <label for="email" class="form-label text-white">E-mail</label>
@@ -10,13 +14,8 @@
     </div>
 
     <div class="mb-3">
-        <label for="nome" class="form-label text-white">Nome Completo</label>
-        <input type="text" class="form-control" id="nome" name="nome" required>
-    </div>
-
-    <div class="mb-3">
-        <label for="mensagem" class="form-label text-white">Mensagem</label>
-        <textarea class="form-control" id="mensagem" name="mensagem" rows="4" required></textarea>
+        <label for="telefone" class="form-label text-white">Telefone</label>
+        <input class="form-control" id="telefone" name="telefone"  required></input>
     </div>
 
     <div class="mb-3">
@@ -28,6 +27,13 @@
             <option value="3">Reclamação</option>
         </select>
     </div>
+
+    <div class="mb-3">
+        <label for="mensagem" class="form-label text-white">Mensagem</label>
+        <textarea class="form-control" id="mensagem" name="mensagem" rows="4" required></textarea>
+    </div>
+
+    
 
     <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
